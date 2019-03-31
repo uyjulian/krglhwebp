eef4c4c50b34979f5c98afa2a1e818126f9237d0
uyjulian <uyjulian@gmail.com>
2019-03-31 10:09:45 -0500

    Saving support

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,13 +1,11 @@
+
 #include <windows.h>
-#include "tp_stub.h"
-#include <tchar.h>
-#include <string.h>
-#include <vector>
+#include <webp/encode.h>
+#include <webp/decode.h>
 #include <memory>
-#include <stdio.h>
+#include "tp_stub.h"
 #define EXPORT(hr) extern "C" __declspec(dllexport) hr __stdcall
 
-#include <webp/decode.h>
 void TVPLoadWEBP(void* formatdata, void *callbackdata, tTVPGraphicSizeCallback sizecallback, tTVPGraphicScanLineCallback scanlinecallback, tTVPMetaInfoPushCallback metainfopushcallback, IStream *src, tjs_int keyidx, tTVPGraphicLoadMode mode)
 {
 	STATSTG stg;
@@ -23,9 +21,10 @@ void TVPLoadWEBP(void* formatdata, void *callbackdata, tTVPGraphicSizeCallback s
 		uint8_t* buffer = WebPDecodeBGRA(data.get(), datasize, &width, &height);
 		if (!buffer) {
 			TVPThrowExceptionMessage(TJS_W("Invalid WebP image (RGBA mode)"));
+			return;
 		}
 		sizecallback(callbackdata, width, height);
-		for( int y = 0; y < height; y++ ) {
+		for(int y = 0; y < height; y++) {
 			void *scanline = scanlinecallback(callbackdata, y);
 			if(!scanline) break;
 			memcpy(scanline, (const void*)&buffer[y*width*sizeof(tjs_uint32)], width*sizeof(tjs_uint32));
@@ -39,8 +38,9 @@ void TVPLoadWEBP(void* formatdata, void *callbackdata, tTVPGraphicSizeCallback s
 		uint8_t* buffer = WebPDecodeYUV(data.get(), datasize, &width, &height, &buffer_u, &buffer_v, &stride, &uv_stride);
 		if (!buffer || !buffer_u || !buffer_v) {
 			TVPThrowExceptionMessage(TJS_W("Invalid WebP image (Grayscale mode)"));
+			return;
 		}
-		for( int y = 0; y < height; y++ ) {
+		for(int y = 0; y < height; y++) {
 			void *scanline = scanlinecallback(callbackdata, y);
 			if(!scanline) break;
 			memcpy(scanline, (const void*)&buffer[y*stride], width);
@@ -85,6 +85,28 @@ void TVPLoadHeaderWEBP(void* formatdata, IStream *src, iTJSDispatch2** dic) {
 	(*dic)->PropSet(TJS_MEMBERENSURE, TJS_W("bpp"), 0, &val, (*dic));
 }
 
+bool TVPAcceptSaveAsWebP(void* formatdata, const ttstr & type, class iTJSDispatch2** dic ) {
+	if( type.StartsWith(TJS_W("webp")) || (type == TJS_W(".webp")) ) return true;
+	return false;
+}
+
+void TVPSaveAsWebP(void* formatdata, void* callbackdata, IStream* dst, const ttstr & mode, tjs_uint width, tjs_uint height, tTVPGraphicSaveScanLineCallback scanlinecallback, iTJSDispatch2* meta ) {
+	int stride = width * sizeof(tjs_uint32);
+	int datasize = stride * height;
+	std::unique_ptr<uint8_t[]> data(new uint8_t[datasize]);
+	for(tjs_uint y = 0; y < height; y++) {
+		memcpy(&(data[y*stride]), scanlinecallback(callbackdata,y), stride);
+	}
+	uint8_t* output;
+	size_t output_size = WebPEncodeLosslessBGRA(data.get(), width, height, stride, &output);
+	if (output_size == 0) {
+		TVPThrowExceptionMessage(TJS_W("Can not encode WebP image"));
+		return;
+	}
+	dst->Write(output, output_size, NULL);
+	WebPFree(output);
+}
+
 BOOL APIENTRY DllMain( HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved ) {
 	return TRUE;
 }
@@ -94,7 +116,7 @@ EXPORT(HRESULT) V2Link(iTVPFunctionExporter *exporter)
 {
 	TVPInitImportStub(exporter);
 
-	TVPRegisterGraphicLoadingHandler( ttstr(TJS_W(".webp")), TVPLoadWEBP, TVPLoadHeaderWEBP, NULL, NULL, NULL );
+	TVPRegisterGraphicLoadingHandler( ttstr(TJS_W(".webp")), TVPLoadWEBP, TVPLoadHeaderWEBP, TVPSaveAsWebP, TVPAcceptSaveAsWebP, NULL );
 
 	GlobalRefCountAtInit = TVPPluginGlobalRefCount;
 	return S_OK;
@@ -103,7 +125,7 @@ EXPORT(HRESULT) V2Link(iTVPFunctionExporter *exporter)
 EXPORT(HRESULT) V2Unlink() {
 	if(TVPPluginGlobalRefCount > GlobalRefCountAtInit) return E_FAIL;
 	
-	TVPRegisterGraphicLoadingHandler( ttstr(TJS_W(".webp")), TVPLoadWEBP, TVPLoadHeaderWEBP, NULL, NULL, NULL );
+	TVPRegisterGraphicLoadingHandler( ttstr(TJS_W(".webp")), TVPLoadWEBP, TVPLoadHeaderWEBP, TVPSaveAsWebP, TVPAcceptSaveAsWebP, NULL );
 
 	TVPUninitImportStub();
 	return S_OK;
