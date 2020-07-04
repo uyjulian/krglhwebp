fc0b742c983eed3093b09ceb875c8aa3ff08e2e1
uyjulian <uyjulian@gmail.com>
2020-07-04 13:23:05 -0500

    Add more encoding options

--- dllmain.cpp
+++ dllmain.cpp
@@ -90,21 +90,132 @@ bool TVPAcceptSaveAsWebP(void* formatdata, const ttstr & type, class iTJSDispatc
 	return false;
 }
 
-void TVPSaveAsWebP(void* formatdata, void* callbackdata, IStream* dst, const ttstr & mode, tjs_uint width, tjs_uint height, tTVPGraphicSaveScanLineCallback scanlinecallback, iTJSDispatch2* meta ) {
+static int WebPIStreamWrite(const uint8_t* data, size_t data_size, const WebPPicture* pic)
+{
+	ULONG bytes_written = 0;
+	((IStream*)pic->custom_ptr)->Write(data, data_size, &bytes_written);
+	return bytes_written == data_size;
+}
+
+#define WEBP_OPTION(parameter, casttype, access_method) \
+	else if (value == TJS_W(#parameter)) \
+	{ \
+		opt_->parameter = (casttype)param[2]->access_method(); \
+	}
+
+void TVPSaveAsWebP(void* formatdata, void* callbackdata, IStream* dst, const ttstr & mode, tjs_uint width, tjs_uint height, tTVPGraphicSaveScanLineCallback scanlinecallback, iTJSDispatch2* meta )
+{
+	if (height == 0 || width == 0)
+	{
+		TVPThrowExceptionMessage(TJS_W("Height or width is zero"));
+		return;
+	}
 	int stride = width * sizeof(tjs_uint32);
 	int datasize = stride * height;
 	std::unique_ptr<uint8_t[]> data(new uint8_t[datasize]);
-	for(tjs_uint y = 0; y < height; y++) {
+	for (tjs_uint y = 0; y < height; y += 1)
+	{
 		memcpy(&(data[y*stride]), scanlinecallback(callbackdata,y), stride);
 	}
-	uint8_t* output;
-	size_t output_size = WebPEncodeLosslessBGRA(data.get(), width, height, stride, &output);
-	if (output_size == 0) {
-		TVPThrowExceptionMessage(TJS_W("Can not encode WebP image"));
+	WebPConfig config;
+	if (!WebPConfigPreset(&config, WEBP_PRESET_DRAWING, 100))
+	{
+		TVPThrowExceptionMessage(TJS_W("Unable to set WebP config preset"));
 		return;
 	}
-	dst->Write(output, output_size, NULL);
-	WebPFree(output);
+	{
+		// Set properties using EnumCallback
+		struct MetaDictionaryEnumCallback : public tTJSDispatch
+		{
+			WebPConfig *opt_;
+			MetaDictionaryEnumCallback( WebPConfig *opt ) : opt_(opt) {}
+			tjs_error TJS_INTF_METHOD FuncCall(tjs_uint32 flag, const tjs_char * membername, tjs_uint32 *hint, tTJSVariant *result, tjs_int numparams, tTJSVariant **param, iTJSDispatch2 *objthis)
+			{ // called from tTJSCustomObject::EnumMembers
+				if (numparams < 3)
+				{
+					return TJS_E_BADPARAMCOUNT;
+				}
+				tjs_uint32 flags = (tjs_int)*param[1];
+				if (flags & TJS_HIDDENMEMBER)
+				{
+					if (result)
+					{
+						*result = (tjs_int)1;
+					}
+					return TJS_S_OK;
+				}
+				// push items
+				ttstr value = *param[0];
+				if (0) {}
+				WEBP_OPTION(lossless, int, AsInteger)
+				WEBP_OPTION(quality, float, AsReal)
+				WEBP_OPTION(method, int, AsInteger)
+				WEBP_OPTION(image_hint, WebPImageHint, AsInteger)
+				WEBP_OPTION(target_size, int, AsInteger)
+				WEBP_OPTION(target_PSNR, float, AsReal)
+				WEBP_OPTION(segments, int, AsInteger)
+				WEBP_OPTION(sns_strength, int, AsInteger)
+				WEBP_OPTION(filter_strength, int, AsInteger)
+				WEBP_OPTION(filter_sharpness, int, AsInteger)
+				WEBP_OPTION(filter_type, int, AsInteger)
+				WEBP_OPTION(autofilter, int, AsInteger)
+				WEBP_OPTION(alpha_compression, int, AsInteger)
+				WEBP_OPTION(alpha_filtering, int, AsInteger)
+				WEBP_OPTION(alpha_quality, int, AsInteger)
+				WEBP_OPTION(pass, int, AsInteger)
+				WEBP_OPTION(show_compressed, int, AsInteger)
+				WEBP_OPTION(preprocessing, int, AsInteger)
+				WEBP_OPTION(partitions, int, AsInteger)
+				WEBP_OPTION(partition_limit, int, AsInteger)
+				WEBP_OPTION(emulate_jpeg_size, int, AsInteger)
+				WEBP_OPTION(thread_level, int, AsInteger)
+				WEBP_OPTION(low_memory, int, AsInteger)
+				WEBP_OPTION(near_lossless, int, AsInteger)
+				WEBP_OPTION(exact, int, AsInteger)
+				WEBP_OPTION(use_delta_palette, int, AsInteger)
+				WEBP_OPTION(use_sharp_yuv, int, AsInteger)
+
+				if (result)
+				{
+					*result = (tjs_int)1;
+				}
+				return TJS_S_OK;
+			}
+		} callback( &config );
+		tTJSVariantClosure clo(&callback, NULL);
+		meta->EnumMembers(TJS_IGNOREPROP, &clo, meta);
+	}
+	if (!WebPValidateConfig(&config))
+	{
+		TVPThrowExceptionMessage(TJS_W("WebP encoding config is invalid"));
+	}
+	WebPPicture pic;
+	if (!WebPPictureInit(&pic))
+	{
+		TVPThrowExceptionMessage(TJS_W("Could not initialize WebP picture"));
+		return;
+	}
+	pic.width = width;
+	pic.height = height;
+	if (!WebPPictureAlloc(&pic))
+	{
+		TVPThrowExceptionMessage(TJS_W("Could not allocate WebP picture"));
+		return;
+	}
+	if (!WebPPictureImportBGRA(&pic, data.get(), stride))
+	{
+		WebPPictureFree(&pic);
+		TVPThrowExceptionMessage(TJS_W("Could not import image to WebP picture"));
+		return;
+	}
+	pic.writer = WebPIStreamWrite;
+	pic.custom_ptr = (void*)dst;
+	int ok = WebPEncode(&config, &pic);
+	WebPPictureFree(&pic);
+	if (!ok)
+	{
+		TVPThrowExceptionMessage(TJS_W("WebP encoding error: %1"), ttstr(pic.error_code));
+	}
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved ) {
